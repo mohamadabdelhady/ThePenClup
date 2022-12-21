dec1ad273ce57222723a6452ae62d7ba2dae067c
mohamadabdelhady <mohamadhady010@gmail.com>
2022-12-21 20:17:43 +0000

    initial commit

---
+++ Layout/Footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center">
+
+</footer>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require 'Layout/header.php'; ?>
+<div class="main">
+    <div class="container">
+
+    </div>
+</div>
+<?php require 'Layout/footer.php'; ?>
